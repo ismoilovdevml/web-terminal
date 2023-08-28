e786fe56fb10898c1de79e1c55cc951e2c88f346
Otabek Ismoilov <107093598+ismoilovdevml@users.noreply.github.com>
2023-08-28 16:11:13 +0500

    Update themes.ts

--- src/components/styles/themes.ts
+++ src/components/styles/themes.ts
@@ -69,22 +69,22 @@ const theme: Themes = {
       },
     },
   },
-  dark: {
-    id: "T_001",
-    name: "dark",
-    colors: {
-      body: "#1D2A35",
-      scrollHandle: "#19252E",
-      scrollHandleHover: "#162028",
-      primary: "#05CE91",
-      secondary: "#FF9D00",
-      text: {
-        100: "#cbd5e1",
-        200: "#B2BDCC",
-        300: "#64748b",
-      },
-    },
-  },
+  // dark: {
+  //   id: "T_001",
+  //   name: "dark",
+  //   colors: {
+  //     body: "#1D2A35",
+  //     scrollHandle: "#19252E",
+  //     scrollHandleHover: "#162028",
+  //     primary: "#05CE91",
+  //     secondary: "#FF9D00",
+  //     text: {
+  //       100: "#cbd5e1",
+  //       200: "#B2BDCC",
+  //       300: "#64748b",
+  //     },
+  //   },
+  // },
   ubuntu: {
     id: "T_006",
     name: "ubuntu",
